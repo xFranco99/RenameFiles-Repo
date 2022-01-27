caf7c04dd71be176c6d3826f05ea2924c75e6f31
xFranco99 <45762539+xFranco99@users.noreply.github.com>
2022-01-27 13:21:18 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace RenameFiles {
+    partial class RenameFiles {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.path = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleziona la cartella";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 83);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(125, 19);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "Ordina con numeri";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(144, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(152, 19);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ordina con nome scelto";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(284, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(302, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Rinomina";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Seleziona un metodo di rinominazione";
+            // 
+            // path
+            // 
+            this.path.AutoSize = true;
+            this.path.Location = new System.Drawing.Point(12, 30);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(0, 15);
+            this.path.TabIndex = 8;
+            // 
+            // RenameFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 145);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "RenameFiles";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RenameFiles";
+            this.Load += new System.EventHandler(this.RenameFiles_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label path;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenameFiles {
+    public partial class RenameFiles : Form {
+        public RenameFiles() {
+            InitializeComponent();
+        }
+
+        private void RenameFiles_Load(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            //apre explorer
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.Description = "Seleziona la cartella dove vuoi rinominare i file";
+            fbd.ShowNewFolderButton = false;
+
+            //scrive nel textBox il path selezionato
+            if (fbd.ShowDialog() == DialogResult.OK) {
+                path.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) {
+
+            //se seleziono il checkbox dei numeri (radioButton1) devo disabilitare la TextBox2
+            if (radioButton1.Checked) {
+                textBox2.Enabled = false;
+            } else {
+                textBox2.Enabled = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+            //se non ho selezionato alcuna cartella avvisami
+            if (path.Text == "") {
+                MessageBox.Show("Inserisci un path!");
+                return;
+            }
+
+            //prendo la dir selezionata e inizializzo a 0 un contatore
+            DirectoryInfo d = new DirectoryInfo(path.Text);
+            int n = 0;
+
+            if (radioButton1.Checked) {
+
+                //lista con tutti i file
+                FileInfo[] infos = d.GetFiles();
+
+                foreach (FileInfo f in infos) {
+
+                    //mi salvo l'estenzione dei file
+                    int lio = f.FullName.LastIndexOf('.');
+                    int len = f.FullName.Length;
+                    string ext = f.FullName.Substring(lio, (len - lio));
+
+                    //rinomino file e aumento contatore
+                    File.Move(f.FullName, d + @"\" + n.ToString() + ext);
+                    n += 1;
+                }
+
+                // avviso fine esecuzione
+                MessageBox.Show("Successo!");
+
+            } else {
+                FileInfo[] infos = d.GetFiles();
+
+                foreach (FileInfo f in infos) {
+                    int lio = f.FullName.LastIndexOf('.');
+                    int len = f.FullName.Length;
+                    string ext = f.FullName.Substring(lio, (len - lio));
+
+                    //Se sono stati immessi caratteri illegali -> avvisami
+                    try {
+                        File.Move(f.FullName, d + @"\" + textBox2.Text.Trim() + " (" + n.ToString() + ")" + ext);
+                    } catch (Exception except) {
+                        MessageBox.Show($"Errore: {except.Message} \nI caratteri " + @" "" <, >, : , "", /, \, |, ?, * "" 
+                            sono illegali per la creazione di file");
+                        return;
+                    }
+
+                    n += 1;
+                }
+
+                MessageBox.Show("Successo!");
+
+            }
+        }
+    }
+}
